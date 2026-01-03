bad2ad35b28ecf26a172284aefb8223f040e9000
Abdunur <zhumaev122@gmail.com>
2026-01-03 12:07:18 +0600

    abdunur

--- experiment — копия/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User, PassengerProfile
-from app.models.aviation import Airport, Airplane, Seat
-from app.models.flight import Flight, Announcement, UserNotification, UserNotificationType
-from app.models.booking import Booking, Ticket, Payment, CheckIn, SeatHold
